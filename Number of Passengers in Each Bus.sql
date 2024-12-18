76a69f3f9d9e21f750c4c26d4618f30c77b48ace
SAI NIHITH IMMANENI <sainihithimmaneni@SAIs-MacBook-Air.local>
2024-12-18 17:56:07 -0600

    Done PR3

---
+++ Number of Passengers in Each Bus.sql
@@ -0,0 +1,10 @@
+
+WITH CTE AS (
+ SELECT   p.passenger_id, p.arrival_time, MIN(b.arrival_time) AS 'btime' FROM Passengers p INNER JOIN Buses b
+ON p.arrival_time <= b.arrival_time
+GROUP BY p.passenger_id
+)
+
+SELECT b.bus_id, COUNT(c.btime) AS passengers_cnt FROM Buses b LEFT JOIN 
+CTE c ON b.arrival_time = c.btime
+GROUP BY b.bus_id ORDER BY b.bus_id
